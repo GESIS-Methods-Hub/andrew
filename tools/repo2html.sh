67375c979ff0d85b115ca40cc3957b166f8289df
Raniere Silva <Raniere.CostadaSilva@gesis.com>
2023-04-18 17:13:21 +0200

    WIP: Try to run quarto inside container

--- tools/repo2html.sh
+++ tools/repo2html.sh
@@ -2,6 +2,17 @@
 
 repo2docker \
     --no-run \
-    --image-name gesis-methods-hub-minimal-example-qmd-rstats \
-    --appendix 'RUN curl -s -L $(curl -s https://quarto.org/docs/download/_prerelease.json | grep -oP "(?<=\"download_url\":\s\")https.*amd64\.tar.gz" | head -n 1) -o /tmp/quarto.tar.gz && mkdir ~/opt && tar -C ~/.local -xvzf /tmp/quarto.tar.gz --strip-components=1' \
+    --user-name methodshub \
+    --image-name methodshub/gesis-methods-hub/minimal-example-qmd-rstats:commit \
+    --appendix 'RUN curl -s -L $(curl -s https://quarto.org/docs/download/_prerelease.json | grep -oP "(?<=\"download_url\":\s\")https.*amd64\.tar.gz" | head -n 1) -o /tmp/quarto.tar.gz && mkdir ~/opt && tar -C ~/.local -xvzf /tmp/quarto.tar.gz --strip-components=1 && rm /tmp/quarto.tar.gz && R --quiet -e "install.packages(\"rmarkdown\")"' \
     https://github.com/GESIS-Methods-Hub/minimal-example-qmd-rstats
+
+# docker run \
+#     --mount type=bind,source=/home/raniere/MethodsHub/raniere-portal/GESIS-Methods-Hub/minimal-example-qmd-rstats,target=/tmp/2methodshub \
+#     gesis-methods-hub-minimal-example-qmd-rstats \
+#     quarto render index.qmd --to md --output ./index.md
+
+docker run -it \
+    --mount type=bind,source=/home/raniere/MethodsHub/raniere-portal/GESIS-Methods-Hub/minimal-example-qmd-rstats,target=/tmp/2methodshub \
+    gesis-methods-hub-minimal-example-qmd-rstats \
+    /bin/bash
