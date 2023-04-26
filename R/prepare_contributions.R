5f5e35c9df29f0d5743e40fce616b8972d41e94e
Raniere Silva <Raniere.CostadaSilva@gesis.org>
2023-04-26 16:26:33 +0200

    Add test for contributions

---
+++ R/prepare_contributions.R
@@ -0,0 +1,20 @@
+library(logger)
+
+library(tidyverse)
+
+pre_process_contributions_list <- function(contribution_row) {
+    regex_match <- str_match(contribution_row, 'https://github.com/(.*).git')
+    user_and_project <- regex_match[2]
+
+    return(user_and_project)
+}
+
+prepare_contributions <- function(all_contributions) {
+    all_contributions$slang <- apply(all_contributions, 1, pre_process_contributions_list)
+    all_contributions <- all_contributions |>
+        separate(slang, c("user_name", "repository_name"), sep='/', remove=FALSE)
+    all_contributions$tmp_path <- str_c("_", all_contributions$slang)
+    all_contributions$https <- str_replace(all_contributions$link, '.git$', '')
+
+    return(all_contributions)
+}
---
+++ tests/testthat/test-prepare_contributions.R
@@ -0,0 +1,29 @@
+library(tidyverse)
+
+test_that("csv with single line", {
+  raw_all_contributions <- tibble(link=c("https://github.com/GESIS-Methods-Hub/minimal-example-md.git"))
+  expected_all_contributions <- tibble(
+    link=c("https://github.com/GESIS-Methods-Hub/minimal-example-md.git"),
+    slang=c("GESIS-Methods-Hub/minimal-example-md"),
+    user_name=c("GESIS-Methods-Hub"),
+    repository_name=c("minimal-example-md"),
+    tmp_path=c("_GESIS-Methods-Hub/minimal-example-md"),
+    https=c("https://github.com/GESIS-Methods-Hub/minimal-example-md"),
+  )
+  all_contributions <- prepare_contributions(raw_all_contributions)
+  expect_equal(all_contributions, expected_all_contributions)
+})
+
+test_that("csv with two line", {
+  raw_all_contributions <- tibble(link=c("https://github.com/GESIS-Methods-Hub/minimal-example-md.git", "https://github.com/GESIS-Methods-Hub/minimal-example-qmd-rstats.git"))
+  expected_all_contributions <- tibble(
+    link=c("https://github.com/GESIS-Methods-Hub/minimal-example-md.git", "https://github.com/GESIS-Methods-Hub/minimal-example-qmd-rstats.git"),
+    slang=c("GESIS-Methods-Hub/minimal-example-md", "GESIS-Methods-Hub/minimal-example-qmd-rstats"),
+    user_name=c("GESIS-Methods-Hub", "GESIS-Methods-Hub"),
+    repository_name=c("minimal-example-md", "minimal-example-qmd-rstats"),
+    tmp_path=c("_GESIS-Methods-Hub/minimal-example-md", "_GESIS-Methods-Hub/minimal-example-qmd-rstats"),
+    https=c("https://github.com/GESIS-Methods-Hub/minimal-example-md", "https://github.com/GESIS-Methods-Hub/minimal-example-qmd-rstats"),
+  )
+  all_contributions <- prepare_contributions(raw_all_contributions)
+  expect_equal(all_contributions, expected_all_contributions)
+})
