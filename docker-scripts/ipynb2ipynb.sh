602b8fc69d89bf1d0cee0b4fbbe77cbd7209cb84
Raniere Silva <Raniere.CostadaSilva@gesis.org>
2023-05-30 14:45:21 +0200

    Remove code not in use

--- docker-scripts/ipynb2ipynb.sh
+++ docker-scripts/ipynb2ipynb.sh
@@ -11,13 +11,4 @@ github_user_name=$2
 github_repository_name=$3
 file2render=$4
 
-git --version
-
-git_hash=$(git rev-parse HEAD)
-
-# If git > 2.25
-# git_date=$(git log -1 --format="%as")
-# else
-git_date=$(git log -1 --format=format:%ad --date=format:%Y-%m-%d)
-
 cp $file2render _output/index.ipynb
--- docker-scripts/ipynb2qmd.sh
+++ docker-scripts/ipynb2qmd.sh
@@ -11,15 +11,6 @@ github_user_name=$2
 github_repository_name=$3
 file2render=$4
 
-git --version
-
-git_hash=$(git rev-parse HEAD)
-
-# If git > 2.25
-# git_date=$(git log -1 --format="%as")
-# else
-git_date=$(git log -1 --format=format:%ad --date=format:%Y-%m-%d)
-
 quarto \
     convert ${file2render} \
     --output index.qmd && \
