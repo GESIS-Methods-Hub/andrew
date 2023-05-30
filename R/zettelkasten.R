2443ac6ad1c867fb3e3027886caea1803c861e44
Raniere Silva <Raniere.CostadaSilva@gesis.org>
2023-05-30 14:09:29 +0200

    Migrate hans.R to R/zettelkasten.R

--- R/zettelkasten.R
+++ R/zettelkasten.R
@@ -81,6 +81,174 @@ create_mega_menu <- function(all_card_row) {
   return(partial)
 }
 
+gallery_1st_level_index_page_template <- "---
+title: '${title}'
+sidebar: false
+toc: false
+anchor-sections: false
+listing:
+  - id: ${slang}-listing
+    template: ../ejs/tiles.ejs
+    contents: listing-contents-${slang}.yml
+---
+
+:::{#${slang}-listing}
+:::
+"
+
+#' Title
+#'
+#' @param subset_data
+#' @param key
+#'
+#' @return
+#' @export
+#'
+#' @examples
+create_1st_level_page <-  function(subset_data, key) {
+  dir.create(key$level_path, recursive = TRUE)
+
+  gallery_index_page <- stringr::str_interp(
+    gallery_1st_level_index_page_template,
+    list(title = key$level, slang= key$level_slang)
+  )
+
+  gallery_index_path <- file.path(key$level_path, "index.md")
+
+  writeLines(gallery_index_page, con = gallery_index_path)
+}
+
+# Create pages for 2nd level
+
+gallery_2nd_level_index_page_template <- "---
+title: '${title}'
+sidebar: false
+toc: false
+anchor-sections: false
+listing:
+  - id: ${slang}-listing
+    template: ../../ejs/methods.ejs
+    contents: listing-contents-${slang}.yml
+---
+
+${abstract}
+
+:::{#${slang}-listing}
+:::
+"
+
+#' Title
+#'
+#' @param all_card_row
+#'
+#' @return
+#' @export
+#'
+#' @examples
+create_2nd_level_page <- function(all_card_row) {
+  dir.create(all_card_row["sublevel_path"], recursive = TRUE)
+
+  gallery_index_page <- stringr::str_interp(
+    gallery_2nd_level_index_page_template,
+    list(
+      title = all_card_row["sublevel"],
+      abstract = all_card_row["abstract"],
+      slang = all_card_row["sublevel_slang"]
+    )
+  )
+
+  gallery_index_path <- file.path(all_card_row["sublevel_path"], "index.md")
+
+  writeLines(gallery_index_page, con = gallery_index_path)
+}
+
+listing_tiles_template <- "- title: ${title}
+  subtitle: ${subtitle}
+  href: ${href}
+  thumbnail: ${thumbnail}"
+
+#' Title
+#'
+#' @param subset_data
+#'
+#' @return
+#' @export
+#'
+#' @examples
+create_listing_root_level <- function(subset_data) {
+  listing_tiles <- subset_data |>
+    dplyr::rowwise() |>
+    dplyr::mutate(
+      listing_tiles = stringr::str_interp(
+        listing_tiles_template,
+        list(
+          title = level,
+          subtitle = subtitle,
+          href = level_path,
+          thumbnail = thumbnail
+        )
+      )
+    ) |>
+    dplyr::pull(listing_tiles) |>
+    stringr::str_flatten(collapse="\n")
+
+  listing_path <- file.path("listing-contents.yml")
+
+  writeLines(listing_tiles, con = listing_path)
+}
+
+#' Title
+#'
+#' @param subset_data
+#' @param key
+#'
+#' @return
+#' @export
+#'
+#' @examples
+create_listing_1st_level <- function(subset_data, key) {
+  listing_tiles <- subset_data |>
+    dplyr::rowwise() |>
+    dplyr::mutate(
+      listing_tiles = stringr::str_interp(
+        listing_tiles_template,
+        list(
+          title = sublevel,
+          subtitle = subtitle,
+          href = sublevel_slang,
+          thumbnail = thumbnail
+        )
+      )
+    ) |>
+    dplyr::pull(listing_tiles) |>
+    stringr::str_flatten(collapse="\n")
+
+  listing_path <- file.path(
+    key$level_path[1],
+    paste0("listing-contents-", key$level_slang[1], ".yml")
+  )
+
+  writeLines(listing_tiles, con = listing_path)
+}
+
+listing_2nd_level_template <- "- path: ../../../GESIS-Methods-Hub/minimal-example-md/index.md"
+
+#' Title
+#'
+#' @param all_card_row
+#'
+#' @return
+#' @export
+#'
+#' @examples
+create_listing_2nd_level <- function(all_card_row) {
+  dir.create(all_card_row["sublevel_path"], recursive = TRUE)
+
+  listing_path <- file.path(all_card_row["sublevel_path"], stringr::str_interp("listing-contents-${slang}.yml", list(slang=all_card_row["sublevel_slang"])))
+
+  writeLines(listing_2nd_level_template, con = listing_path)
+}
+
 #' Title
 #'
 #' @param all_card_filename
@@ -133,4 +301,25 @@ generate_card_files <- function(all_cards_filename = 'zettelkasten.csv') {
   ) |>
     writeLines(con = create_mega_menu_path)
 
+
+  all_cards |>
+    dplyr::group_by(level, level_slang, level_path) |>
+    dplyr::group_walk(create_1st_level_page)
+
+  all_cards |>
+    dplyr::filter(!is.na(sublevel)) |>
+    apply(1, create_2nd_level_page)
+
+  all_cards |>
+    dplyr::filter(is.na(sublevel)) |>
+    create_listing_root_level()
+
+  all_cards |>
+    dplyr::filter(!is.na(sublevel)) |>
+    dplyr::group_by(level_path, level_slang) |>
+    dplyr::group_walk(create_listing_1st_level)
+
+  all_cards |>
+    dplyr::filter(!is.na(sublevel)) |>
+    apply(1, create_listing_2nd_level)
 }
