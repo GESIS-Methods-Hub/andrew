0db9f34cef2fff27546f84634af8e0667f62ab42
Raniere Silva <Raniere.CostadaSilva@gesis.org>
2023-05-30 14:44:58 +0200

    Add variable quarto_version

--- docker-scripts/qmd2md.sh
+++ docker-scripts/qmd2md.sh
@@ -20,6 +20,8 @@ git_hash=$(git rev-parse HEAD)
 # else
 git_date=$(git log -1 --format=format:%ad --date=format:%Y-%m-%d)
 
+quarto_version=$(quarto --version)
+
 quarto \
     render ${file2render} \
     --to md \
@@ -31,6 +33,7 @@ quarto \
     --variable "github_repository_name:${github_repository_name}" \
     --variable "git_hash:${git_hash}" \
     --variable "git_date:${git_date}" \
+    --variable "quarto_version:${quarto_version}" \
     --variable "source_filename:${file2render}" && \
     cp index.md _output/index.md
 
