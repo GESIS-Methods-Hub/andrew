a25c7b243f1de4df557a8a443b6dccac2aa9c952
Raniere Silva <Raniere.CostadaSilva@gesis.org>
2023-07-17 09:43:57 +0200

    Use environment variables to share data between R and Docker
    Closes https://github.com/GESIS-Methods-Hub/methodshub/issues/96

--- inst/docker-scripts/docx2md.sh
+++ inst/docker-scripts/docx2md.sh
@@ -4,12 +4,7 @@
 #
 # Syntax:
 #
-# docx2md.sh github_https github_user_name github_repository_name file2render
-
-github_https=$1
-github_user_name=$2
-github_repository_name=$3
-file2render=$4
+# docx2md.sh
 
 dirname2render=$(dirname ${file2render})
 basename2render=$(basename ${file2render})
--- inst/docker-scripts/md2ipynb.sh
+++ inst/docker-scripts/md2ipynb.sh
@@ -4,12 +4,7 @@
 #
 # Syntax:
 #
-# md2ipynb.sh github_https github_user_name github_repository_name file2render
-
-github_https=$1
-github_user_name=$2
-github_repository_name=$3
-file2render=$4
+# md2ipynb.sh
 
 dirname2render=$(dirname ${file2render})
 basename2render=$(basename ${file2render})
