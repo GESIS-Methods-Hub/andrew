32fe2ba731ab3a878d4e31b44b22892031e79998
Raniere Silva <Raniere.CostadaSilva@gesis.org>
2023-07-18 14:18:47 +0200

    Change screenshot size to Full HD

--- img/screenshot.sh
+++ img/screenshot.sh
@@ -15,10 +15,12 @@ then
   exit 1
 fi
 
-$1 --headless --screenshot=img/methodshub-root.png demo/_site/index.html
+full_args="--headless -window-size=1920,1080"
 
-$1 --headless --screenshot=img/methodshub-1st-level.png demo/_site/gallery/fundamentals/index.html
+$1 $full_args --screenshot=img/methodshub-root.png demo/_site/index.html
 
-$1 --headless --screenshot=img/methodshub-2nd-level.png demo/_site/gallery/fundamentals/basic-setup/index.html
+$1 $full_args --screenshot=img/methodshub-1st-level.png demo/_site/gallery/fundamentals/index.html
 
-$1 --headless --screenshot=img/methodshub-content.png demo/_site/GESIS-Methods-Hub/minimal-example-md/index/index.html
+$1 $full_args --screenshot=img/methodshub-2nd-level.png demo/_site/gallery/fundamentals/basic-setup/index.html
+
+$1 $full_args --screenshot=img/methodshub-content.png demo/_site/GESIS-Methods-Hub/minimal-example-md/index/index.html
