5d2d2ea20e7b1245ad3b5d76eb0ba9b8b4aaee9d
Raniere Silva <Raniere.CostadaSilva@gesis.org>
2023-07-18 08:58:18 +0200

    Add script to render Markdown into PDF

---
+++ inst/docker-scripts/md2pdf.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+# Convert Markdown to PDF
+#
+# Syntax:
+#
+# md2pdf.sh
+
+dirname2render=$(dirname ${file2render})
+basename2render=$(basename ${file2render})
+
+input_dirname=~/methodshub/$dirname2render/${basename2render%.*}
+input_basename=index.md
+
+quarto_version=$(quarto --version)
+
+cd $input_dirname
+
+quarto \
+    render ${input_basename} \
+    --to pdf \
+    --output index.pdf
