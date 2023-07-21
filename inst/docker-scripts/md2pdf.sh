c068c3e9e577159868bd5df924d248648cd6d6d3
Raniere Silva <Raniere.CostadaSilva@gesis.org>
2023-07-21 16:00:08 +0200

    Fix Markdown to PDF

--- inst/docker-scripts/md2pdf.sh
+++ inst/docker-scripts/md2pdf.sh
@@ -9,14 +9,14 @@
 dirname2render=$(dirname ${file2render})
 basename2render=$(basename ${file2render})
 
-output_dirname=$output_location/$dirname2render/${basename2render%.*}
+input_dirname=${basename2render%.*}
 input_basename=index.md
 
-quarto_version=$(quarto --version)
+cd ~/methodshub/$input_dirname
 
-cd $input_dirname
+quarto --version
 
 quarto \
-    render ${input_basename} \
+    render $input_basename \
     --to pdf \
     --output index.pdf
