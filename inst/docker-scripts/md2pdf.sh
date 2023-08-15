297f008598176d26093b2aa36fe0221c5e562e47
Raniere Silva <Raniere.CostadaSilva@gesis.org>
2023-08-15 14:20:31 +0200

    Change pdf engine to LuaLaTeX
    Closes https://github.com/GESIS-Methods-Hub/andrew/issues/193

--- inst/docker-scripts/md2pdf.sh
+++ inst/docker-scripts/md2pdf.sh
@@ -16,7 +16,25 @@ cd ~/andrew/$input_dirname
 
 quarto --version
 
+cat > _quarto.yml <<EOF
+format:
+  pdf:
+    pdf-engine: lualatex
+    papersize: a4
+    geometry:
+      - top=25mm
+      - bottom=20mm
+      - left=25mm
+      - right=25mm
+    fontsize: '10'
+    classoption:
+      - DIV=10
+      - numbers=noendperiod
+EOF
+
 quarto \
     render $input_basename \
     --to pdf \
     --output index.pdf
+
+rm _quarto.yml
