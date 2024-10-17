192cbb1a521f80746acae097a2d7aa134b167372
dehnejn <dehnejn@gesis.org>
2024-10-17 16:54:39 +0200

    Remove unnecessary files from tracking and add to .gitignore

--- demo/status.md
+++
@@ -1,6 +0,0 @@
----
-title: "Status Page"
-comments: false
----
-
-{{< include _report-table.md >}}
--- inst/pandoc-filters/leave-license-field.lua
+++
@@ -1,6 +0,0 @@
-function Meta(meta)
-  if meta.license then
-    meta.license = "CC BY-NC 4.0"
-  end
-  return meta
-end
